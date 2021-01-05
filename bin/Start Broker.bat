2a60386f530e03664ea3db0eb5db4b39d0b2d9a1
yuqi <mack26@126.com>
2021-01-05 18:26:38 +0800

    修改示例

--- bin/Start Broker.bat
+++ bin/Start Broker.bat
@@ -1,3 +1,4 @@
+rem set JAVA_HOME=D:\Program Files\Java\jdk1.8.0_201
 d:
-cd \rocketmq-all-4.7.1-bin-release
+cd \software\rocketmq-all-4.7.1-bin-release
 .\bin\mqbroker.cmd -n localhost:9876 autoCreateTopicEnable=true
